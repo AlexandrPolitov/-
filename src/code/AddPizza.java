967748c2bc7ab3a2d56e0b83c00446964fbc3380
Александр Политов <mister_politov@rambler.ru>
2019-05-19 13:29:17 +0300

    sme

--- src/code/AddPizza.java
+++ src/code/AddPizza.java
@@ -2,8 +2,6 @@ package code;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -11,43 +9,34 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 
-public class AddPizza extends mainPage{
+public class AddPizza extends mainPage {
 
     @FXML
     Button btnAddPhoto;
-
     @FXML
     Button btnAddPizza;
-
     @FXML
     TextField ProductName;
-
     @FXML
     TextField ProductPrice;
-
     @FXML
     TextArea ProductDescription;
-
     @FXML
     ImageView logo;
 
-    @FXML
-    GridPane container;
-
-    public static Image image;
+    public static String image = "";
     public static String name = "";
     public static String price = "";
     public static String description = "";
 
     // проверка, нажал ли пользователь
-    static boolean check = false;
+    public static boolean check = false;
 
     // Событие добавления фото
     public void addPhoto(MouseEvent mouseEvent) {
@@ -58,33 +47,33 @@ public class AddPizza extends mainPage{
         File file = fc.showOpenDialog(b.getScene().getWindow());
         try {
             Image candidate = SwingFXUtils.toFXImage(ImageIO.read(file), null);
-            if (candidate.getHeight() > 300 || candidate.getWidth() > 300)
-            {
+            if (candidate.getHeight() > 300 || candidate.getWidth() > 300) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Ошибка");
                 alert.setContentText("Требуемый размер изображения: не более 300 x 300 пикселей");
                 alert.show();
                 return;
             }
-            image = candidate;
             logo.setImage(candidate);
-
+            image = "file:/" + file.getPath();
         }
-        catch (IOException e)
-        {
+        catch (IOException e) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Ошибка");
             alert.setContentText("Невозможно открыть изображение");
+            alert.show();
         }
-
-
     }
 
     // Событие добавления пиццы
     public void addPizza(MouseEvent mouseEvent) throws Exception {
-        name = ProductName.getText();
-        price = ProductPrice.getText();
-        description = ProductDescription.getText();
+        if (ProductName.getText().equals("")) name = "Без названия";
+        else name = ProductName.getText();
+        if (ProductPrice.getText().equals("")) price = "0";
+        else price = ProductPrice.getText();
+        if (ProductDescription.getText().equals("")) description = "";
+        else description = ProductDescription.getText();
+        check = true;
         Stage stage = (Stage) btnAddPhoto.getScene().getWindow();
         stage.close();
     }
